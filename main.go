37334f35c58adc9bc0e036cc2f808d79a9bfa730
sergge1 <49363121+sergge1@users.noreply.github.com>
2019-08-17 15:22:19 +0300

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/bhoriuchi/go-bunyan/bunyan"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/sergge1/txwifi/iotwifi"
+	"github.com/sergge1/txwifi/tree/develop/iotwifi"
 )
 
 // ApiReturn structures a message for returned API calls.
